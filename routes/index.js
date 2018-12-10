503e7016f08d8f394c7a189529df53f513d170af
yaoyuanming <yaoyuanming0601@gmail.com>
2018-12-10 18:03:34 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,18 @@ const testRoutes = require("./test");
 const homeRoutes = require("./home");
 const signupRoutes = require("./signup");
 const productRoutes = require("./product");
+const myaccountRoutes = require("./myaccount");
+const cartRoutes = require("./cart");
+const wishlishRoutes = require("./wishlish")
 
 const constructorMethod = app => {
     app.use("/test", testRoutes),
     app.use("/", homeRoutes),
     app.use("/signup", signupRoutes),
-    app.use("/", productRoutes)
+    app.use("/", productRoutes),
+    app.use("/myaccount",myaccountRoutes),
+    app.use("/cart",cartRoutes),
+    app.use("/wishlish",wishlishRoutes)
 }
 
 module.exports = constructorMethod;
