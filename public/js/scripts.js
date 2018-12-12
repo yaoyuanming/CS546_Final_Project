1674a6028e4dac7dfb641688a1b1113b6a3fbdfb
YingdongLi <lyd941026@gmail.com>
2018-12-12 13:39:04 -0500

    searchbar on products view

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,35 @@
 // login modal
 $("#login-button").click(function() {
     $("#login-modal").modal("show");
+});
+
+$(document).ready(function() {
+    $("#searchInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#productLists #single").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        })
+    })
 })
+
+/*function search() {
+    var input = document.getElementById("searchInput");
+    var filter = input.value.toLowerCase();
+    var allCards = document.getElementById("productLists");
+    var results = document.getElementsById("single");
+    for (i = 0 ; i < results.length; i ++) {
+        var title = results[i].getElementByClassName("card-title");
+        var category = results[i].getElementById("category");
+        if(title || category) {
+            titletext = title.textContent.toLowerCase();
+            category = category.textContent.toLowerCase();
+            if(titletext.indexOf(filter) > -1 || category.indexOf(filter) > -1) {
+                results[i].style.display = "";
+            } else {
+                results[i].style.display = "none";
+            }
+        }
+    }
+}*/
+
 
